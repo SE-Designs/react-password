ccbcb804a7c43193e12051dc213ec9983d330e53
mixture <64894804+SE-Designs@users.noreply.github.com>
2025-03-23 15:04:55 +0500

    upd: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>
 );
